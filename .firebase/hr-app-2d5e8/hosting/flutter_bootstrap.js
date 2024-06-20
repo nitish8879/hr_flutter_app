6ab6a663db0d40fab3bd54da299f222b5530254e
Nitish Gupta <nitish_gupta@tijoree.money>
2024-06-20 13:09:11 +0530

    done

--- .firebase/hr-app-2d5e8/hosting/flutter_bootstrap.js
+++ .firebase/hr-app-2d5e8/hosting/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3969206707"
+    serviceWorkerVersion: "2278872552"
   }
 });
